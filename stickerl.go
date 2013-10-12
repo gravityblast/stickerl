ee331b9a112845c34b838e9929529c87c576cec7
Andrea Franz <andrea@gravityblast.com>
2013-10-12 21:08:24 +0100

    refactoring

--- stickerl.go
+++ stickerl.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-  "log"
+  "os"
   "fmt"
+  "flag"
   "strings"
   "net/http"
   "image/png"
@@ -13,18 +14,16 @@ import (
 
 const VERSION = "0.1.0"
 
-var baseUrl string
+var (
+  baseUrl string
+  env     string
+  app     *traffic.Router
+)
 
-func init() {
-  var ok bool
-  baseUrl, ok = traffic.GetVar("base_redirect_url").(string)
-  if !ok || baseUrl == "" {
-    log.Fatal("Base Redirect URL is blank. Set the environment variable TRAFFIC_BASE_REDIRECT_URL")
-  }
-
-  if !strings.HasSuffix(baseUrl, "/") {
-    baseUrl = fmt.Sprintf("%s/", baseUrl)
-  }
+func usage() {
+  fmt.Println("USAGE:")
+  fmt.Printf("  %s [OPTIONS] BASE_URL\n", os.Args[0])
+  flag.PrintDefaults()
 }
 
 func rootHandler(w traffic.ResponseWriter, r *http.Request) {
@@ -49,7 +48,23 @@ func qrcodesHandler(w traffic.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  app := traffic.New()
+  flag.StringVar(&env, "e", "production", "Traffic ENV")
+  flag.Parse()
+  args := flag.Args()
+
+  if len(args) != 1 {
+    usage()
+    os.Exit(1)
+  }
+
+  baseUrl = args[0]
+  if !strings.HasSuffix(baseUrl, "/") {
+    baseUrl = fmt.Sprintf("%s/", baseUrl)
+  }
+
+  traffic.SetVar("env", env)
+
+  app = traffic.New()
   app.Get("/", rootHandler)
   app.Get("/qrcodes/:code", qrcodesHandler)
   app.Run()
